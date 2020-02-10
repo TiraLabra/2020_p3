a05661f1844577ecdcd56de64c517cb3caa949bf
saskeli <saskeli@users.noreply.github.com>
2020-02-10 14:50:17 +0200

    Update fuu.js

--- assets/fuu.js
+++ assets/fuu.js
@@ -18,7 +18,7 @@ const timing = {
     "end": {"date": "2020-03-06T23:59:59+02:00", "en": "Final submission", "fi": "Loppupalautus"}
 };
 
-const doodleSent = false;
+const doodleSent = true;
 
 /*
 Available fields for TAs:
